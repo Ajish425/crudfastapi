aaf6375b75914f749a085a283ef427d0dbc7f9d0
Ajish425 <ajishr365@outlook.com>
2026-02-26 01:27:58 +0530

    jinja2

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from sqlmodel import Field, Session, SQLModel, create_engine, select
+from pydantic import BaseModel
 
 app = FastAPI()
-
-
-
-
 templates = Jinja2Templates(directory="templates")
 
+@app.get("/", response_class=HTMLResponse)
+async def form_page(request: Request):
+    return templates.TemplateResponse("item.html", {"request": request})
+@app.post("/submit", response_class=HTMLResponse)
+async def meta_data( request: Request,nm: str = Form(...)):
 
-@app.get("/items/{id}", response_class=HTMLResponse)
-async def read_item(request: Request, id: str):
-    return templates.TemplateResponse(
-        request=request, name="item.html", context={"id": id}
-    )
+    data ={"request": request,"name":nm}
+    return templates.TemplateResponse("item.html",data)
+
+
+
