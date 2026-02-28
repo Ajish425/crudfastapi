42570a60a6ec9752b83d558afa63cffc7a5473eb
Ajish425 <ajishr365@outlook.com>
2026-02-28 21:42:36 +0530

    crud

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from typing import Annotated
 app = FastAPI()
 
 class Zepto(SQLModel, table=True):
-    #id: int | None = Field(default=None, primary_key=True)
+
     category: str = Field(index=True, primary_key=True)
     name: str = Field(index=True)
     mrp:float = Field(index=True)
@@ -16,6 +16,18 @@ class Zepto(SQLModel, table=True):
     weight_in_gms:int = Field(index=True)
     out_of_stock:bool = Field(index=True)
     quantity:int = Field(index=True)
+
+class ZeptoPut(SQLModel):
+
+        category: str | None = None
+        name: str | None = None
+        mrp: float | None = None
+        discount_percent: int | None = None
+        available_quantity: int | None = None
+        discounted_selling_price: float | None = None
+        weight_in_gms: int | None = None
+        out_of_stock: bool | None = None
+        quantity: int | None = None
 
 
 sqlite_url = "postgresql://postgres:12345@localhost:5432/sales_data"
@@ -42,12 +54,35 @@ async def get_zepto(category: str, session: SessionDep):
         raise HTTPException(status_code=404, detail="zepto not found")
     return z
 
+@app.get("/zepto/")
+async def get_zepto(session: SessionDep):
+    result = session.exec(select(Zepto)).all()
+
+    # Fetch all results from the executed statement
+
+    if not result:
+        raise HTTPException(status_code=404, detail="zepto not found")
+    return result
+
 @app.delete("/zepto/delete/{category}")
 async def delete_zepto(session: SessionDep, category:str):
     zepto = session.get(Zepto, category)
     session.delete(zepto)
     session.commit()
     return zepto
+
+@app.put("/zepto/update/{category}" ,response_model=Zepto)
+
+async def update_zepto(category:str, zepto: ZeptoPut, session: SessionDep):
+    zepto_put = session.get(Zepto, category)
+    if not zepto_put:
+        raise HTTPException(status_code=404, detail="zepto not found")
+    zepto_data = zepto.model_dump(exclude_unset=True)
+    zepto_put.sqlmodel_update(zepto_data)
+    session.add(zepto_put)
+    session.commit()
+    session.refresh(zepto_put)
+    return zepto_put
 
 
 
